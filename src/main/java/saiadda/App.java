e5e8a53a9dfc7109763f49256b1b6222ef1702f0
saikrishna <saikrish0870@gmail.com>
2021-03-25 14:56:58 +0530

    completed

---
+++ src/main/java/saiadda/App.java
@@ -0,0 +1,13 @@
+package saiadda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! and how are you" );
+    }
+}
